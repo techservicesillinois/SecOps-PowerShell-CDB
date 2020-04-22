9031571c1c4f5dc211a7b24880d847ab5d0712f6
MKletz <michael.kletz.27@gmail.com>
2020-04-22 14:34:05 -0500

    Fixed a bug where stored credentials would not update URI autofill

--- UofICDB/UofICDB.psm1
+++ UofICDB/UofICDB.psm1
@@ -9,12 +9,13 @@ Get-ChildItem -Path $FunctionPath -Filter "*.ps1" -Recurse | ForEach-Object -Pro
 [String]$SettingsPath = Join-Path -Path $PSScriptRoot -ChildPath 'Settings.json'
 $Script:Settings = Get-Content -Path $SettingsPath | ConvertFrom-Json
 
+$Script:SubClassURIs = @{}
+
 [String]$Script:SavedCredsDir = Join-Path -Path $([System.Environment]::GetFolderPath(28)) -ChildPath 'PSCDBAuth.txt'
 if(Test-Path -Path $Script:SavedCredsDir){
     $Script:Authorization = Get-Content -Path $Script:SavedCredsDir | ConvertTo-SecureString | ConvertFrom-SecureString -AsPlainText
+    Update-CDBSubclassUris
 }
 else{
     $Script:Authorization = [String]::Empty
 }
-
-$Script:SubClassURIs = @{}
