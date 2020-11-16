437d2fa6f1ad636b5d6ddfd9bba79823abaa4908
MKletz <michael.kletz.27@gmail.com>
2020-11-16 11:08:32 -0600

    action dirs

--- .github/workflows/PublishToGallery.ps1
+++ .github/workflows/PublishToGallery.ps1
@@ -1,6 +1,6 @@
 try{
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    Publish-Module -Path ".\UofICDB" -Repository PSGallery -NuGetApiKey $ENV:NuGetApiKey -Force
+    Publish-Module -Path '.\src\UofICDB' -Repository PSGallery -NuGetApiKey $ENV:NuGetApiKey -Force
 }
 catch{
     throw $_
